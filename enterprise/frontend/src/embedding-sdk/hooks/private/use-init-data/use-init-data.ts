ea8740d41ce8dc8529f860656175c611a468dd0d
Timofei <timofei@metabase.com>
2025-07-18 14:25:08 +0400

    Always call `registerVisualizationsOnce`

--- enterprise/frontend/src/embedding-sdk/hooks/private/use-init-data/use-init-data.ts
+++ enterprise/frontend/src/embedding-sdk/hooks/private/use-init-data/use-init-data.ts
@@ -53,6 +53,8 @@ export const useInitData = ({
       return;
     }
 
+    registerVisualizationsOnce();
+
     const isAuthUninitialized =
       store && store.getState().sdk.loginStatus.status === "uninitialized";
 
@@ -61,8 +63,6 @@ export const useInitData = ({
     }
 
     hasBeenInitialized.current = true;
-
-    registerVisualizationsOnce();
 
     dispatch(initAuth(authConfig));
 
